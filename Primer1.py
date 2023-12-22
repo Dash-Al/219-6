f6b0324a1d0135d5954dc37c4fc4f0cb02d3fa5f
Дарья <125445507+Dash-Al@users.noreply.github.com>
2023-12-22 21:16:42 +0300

    Update Primer1.py

--- Primer1.py
+++ Primer1.py
@@ -10,3 +10,6 @@ if __name__ == "__main__":
     for file_name in glob.glob("*.txt"):
         new_path = os.path.join("archive", file_name)
         shutil.move(file_name, new_path)
+if __name__ == "__main__":
+main()
+ 
